08f711199c245eacc9f8324f8047117746d4bd4a
Leandro <114453601+Lauriindo@users.noreply.github.com>
2024-03-04 21:41:43 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+value1 = input("Qual foi sua primeira nota?")
+value2 = input("Qual foi sua segunda nota?")
+value3 = value1 + value2
+if value3 >= 6:{
+    print("Aprovado")
+}
+else:{
+    print("Reprovado")
+}
